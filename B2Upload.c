83200bcb29d771bcddb2e9ce04bc4a12a5fccb8e
Tommy Lane <shopping-list@km4lvw.com>
2020-03-21 20:33:42 -0500

    Gets valid auth token back

--- B2Upload.c
+++ B2Upload.c
@@ -6,13 +6,17 @@
 
 #include <curl/curl.h>
 #define GLOBAL_BUFF_SIZE 4096
-uint8_t BUFFER[GLOBAL_BUFF_SIZE];
+char BUFFER[GLOBAL_BUFF_SIZE];
 size_t BUFFEROffset = 0;
+
+#define LOG_ERROR(...) fprintf(stderr, __VA_ARGS__)
+//#define LOG_BASE(...) LOG_ERROR()
 
 const char* B2UploadKeyId = "00148165f5a47f20000000009";
 const char* B2UploadSecret = "K001s0e//xC2qV0q/Y8nFBh3bwTbVkg";
+const char* B2apiNameVerURL = "/b2api/v5/"
 
-const char* INJEST_BUCKET = "com-kml4vw-injest";
+const char* INJEST_BUCKET = "com-km4lvw-injest";
 
 void setBuffer(uint8_t* data, size_t len)
 {
@@ -46,10 +50,29 @@ static size_t curlFillBuffer(uint8_t* ptr, size_t size, size_t nmemb, void* stre
 	return size*nmemb;
 }
 
+char* getDynamicStringCopy(char* ntString)
+{
+	char* ret = NULL;
+	size_t len = strlen(ntString) + 1;
+	ret = malloc(len);
+	if (ret != NULL)
+	{
+		memset((void*) ret, 0, len);
+		snprintf(ret, len, "%s",ntString);
+	}
+	return ret;
+}
+
 int main(int argc, char** argv, char** envp)
 {
 	CURL* curl;
 	CURLcode response = 0;
+	struct curl_slist *chunk = NULL;
+	char* fullAuthString = NULL;
+	char* authTok = NULL;
+	char* apiUrl = NULL;
+	char* postUrlResponse = NULL;
+	char* postUrl = NULL;
 	char* cp;
 	memset((void*) BUFFER, 0, sizeof(BUFFER));
 	snprintf(BUFFER, sizeof(BUFFER), "%s:%s", B2UploadKeyId, B2UploadSecret);
@@ -65,23 +88,105 @@ int main(int argc, char** argv, char** envp)
 		response = curl_easy_perform(curl);
 		if (response != CURLE_OK)
 		{
-			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(response));
+			LOG_ERROR("curl_easy_perform() failed: %s\n", curl_easy_strerror(response));
 		}
 		else
 		{
-			cp = strstr(BUFFER, "authorizationToken");
-			if (cp != NULL)
-			{	
-				cp = strstr(cp, ":");
-				cp++;
-				cp = strtok(cp, "\"");
-				cp = strtok(NULL, "\"");
-				sscanf(cp, "%s", BUFFER);
-				printf("authorizationToken: \n%s\n", BUFFER);
+			fullAuthString = getDynamicStringCopy(BUFFER);
+			LOG_ERROR("fullAuthString: %s", fullAuthString);
+			if (fullAuthString != NULL)
+			{
+				cp = strstr(BUFFER, "authorizationToken");
+				if (cp != NULL)
+				{	
+					cp = strstr(cp, ":");
+					cp++;
+					cp = strtok(cp, "\"");
+					cp = strtok(NULL, "\"");
+					if (cp != NULL)
+					{
+						//sscanf(cp, "%s", BUFFER);
+						snprintf(BUFFER, sizeof(BUFFER), "%s", cp);
+						printf("authorizationToken: \n%s\n", BUFFER);
+						authTok = getDynamicStringCopy(BUFFER);
+						if (authTok != NULL)
+						{
+							//fullAuthString valid, and authTok valid.
+							memset((void*) BUFFER, 0, sizeof(BUFFER));
+							memcpy((void*) BUFFER, fullAuthString, strlen(fullAuthString));
 
+							//Get api url from authString.
+							cp = strstr(BUFFER, "apiUrl");
+							if (cp != NULL)
+							{	
+								cp = strstr(cp, ":");
+								cp++;
+								cp = strtok(cp, "\"");
+								cp = strtok(NULL, "\"");
+								if (cp != NULL)
+								{
+									//sscanf(cp, "%s", BUFFER);
+									snprintf(BUFFER, sizeof(BUFFER), "%s", cp);
+									printf("apiUrl: \n%s\n", BUFFER);
+									apiUrl = getDynamicStringCopy(BUFFER);
+									if (apiUrl != NULL)
+									{
+										curl_easy_setopt(curl, CURLOPT_USERPWD, NULL);
+
+										snprintf(BUFFER, sizeof(BUFFER), "%s%s/%s", apiUrl, B2apiNameVerURL, "b2_get_upload_url");
+										curl_easy_setopt(curl, CURLOPT_URL, BUFFER);
+										memset((void*) BUFFER, 0, sizeof(BUFFER));
+
+										chunk = curl_slist_append(chunk, "Accept:");
+
+										snprintf(BUFFER, sizeof(BUFFER) "%s: %s", "Authorization", authTok);
+										chunk = curl_slist_append(chunk, BUFFER);
+										curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+
+										memset((void*) BUFFER, 0, sizeof(BUFFER));
+										response = curl_easy_perform();
+
+										curl_slist_free_all(chunk);
+										chunk = NULL;
+										if (response != CURLE_OK)
+										{
+											LOG_ERROR("curl_easy_perform() failed: %s\n", curl_easy_strerror(response));
+										}
+										else
+										{
+											//Got back valid response.
+											long responseCode;
+											curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);
+											if (responseCode < 300 && responseCode > 199)
+											{
+												//Valid url recieved.
+												postUrlResponse = getDynamicStringCopy(BUFFER);
+												if (postUrlResponse != NULL)
+												{
+													memset((void*) BUFFER, 0, sizeof(BUFFER));
+
+													
+													
+													free(postUrlResponse);
+												}
+											}
+										}
+
+										free(apiUrl);
+									}
+								}
+							}
+							free(authTok);
+							authTok = NULL;
+						}
+					}
+				}
+				free(fullAuthString);
+				fullAuthString = NULL;
 			}
 		}
-		printf("cleaning up.\n");
+
+		LOG_ERROR("cleaning up.\n");
 		curl_easy_cleanup(curl);
 	}
 	/* code */
