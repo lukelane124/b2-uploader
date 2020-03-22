8b65fb01a08baedec752b9e27dcdda12554d05f1
Tommy Lane <shopping-list@km4lvw.com>
2020-03-22 12:58:17 -0500

    Cleaning up useability of string obfuscator.

--- stringObs.c
+++ stringObs.c
@@ -7,34 +7,50 @@
 
 #include "c_defs.h"
 
+int returnCode = 0;
+
 const char* UsageCopyWrite = "Copyright: Tommy Lane (L&L Operations) 2020";
-const char* key = "085f4048015902475b4263661e3b5d080f1559002362407f443b742d260e15";
+char* programName = NULL;
 
 char secret[40];
 char temp[5];
 
+void Usage(void)
+{
+	fprintf(stderr, "Usage:\n\t%s <secret key for obfuscation>\n\n\n", programName);
+	returnCode = -1;
+}
+
 int main(int argc, char** argv, char** envp)
 {
+	programName = argv[0];
 	char* inputString = argv[1];
-	char *pos = key;
-	for (int i = 0; i < strlen(inputString); i++)
+	// char *pos = key;
+	if (argc < 2)
 	{
-		printf("%02x", (char)(inputString[i] ^ (UsageCopyWrite[i%strlen(UsageCopyWrite)]) ));
+		Usage();
 	}
-	printf("\n");
+	else
+	{
+		for (int i = 0; i < strlen(inputString); i++)
+		{
+			printf("%02x", (char)(inputString[i] ^ (UsageCopyWrite[i%strlen(UsageCopyWrite)]) ));
+		}
+		printf("\n");
 
-	memset((void*)secret, 0, sizeof(secret));
-	LOG_DEBUG("decoding secret\n");
-	for (size_t count = 0; count < sizeof(secret); count++) {
-        sscanf(pos, "%2hhx", &secret[count]);
-        secret[count] = (secret[count] ^ (UsageCopyWrite[(count)%strlen(UsageCopyWrite)]));
-        pos += 2;
-        if (!*pos)
-        {
-        	break;
-        }
-    }
-	printf("secret: %s\n", secret);
-
+		// memset((void*)secret, 0, sizeof(secret));
+		// LOG_DEBUG("decoding secret\n");
+		// for (size_t count = 0; count < sizeof(secret); count++) {
+	 //        sscanf(pos, "%2hhx", &secret[count]);
+	 //        secret[count] = (secret[count] ^ (UsageCopyWrite[(count)%strlen(UsageCopyWrite)]));
+	 //        pos += 2;
+	 //        if (!*pos)
+	 //        {
+	 //        	break;
+	 //        }
+	 //    }
+		// printf("secret: %s\n", secret);
+	}
+	return returnCode;
 
 }
